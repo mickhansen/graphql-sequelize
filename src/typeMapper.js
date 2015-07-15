cd682d1231f1e5a342559abab84fd830fd6a5e9f
Mick Hansen <mick.kasper.hansen@gmail.com>
2015-07-15 16:48:41 +0200

    Handle boolean types

--- src/typeMapper.js
+++ src/typeMapper.js
@@ -1,8 +1,10 @@
 import Sequelize from 'sequelize';
-import { GraphQLInt, GraphQLString } from 'graphql';
+import { GraphQLInt, GraphQLString, GraphQLBoolean } from 'graphql';
 
 export function toGraphQL(sequelizeType) {
-  if (sequelizeType instanceof Sequelize.INTEGER) {
+  if (sequelizeType instanceof Sequelize.BOOLEAN) {
+    return GraphQLBoolean;
+  } else if (sequelizeType instanceof Sequelize.INTEGER) {
     return GraphQLInt;
   } else if (
     sequelizeType instanceof Sequelize.STRING ||
@@ -11,6 +13,6 @@ export function toGraphQL(sequelizeType) {
   ) {
     return GraphQLString;
   } else {
-    throw new Error(`Unable to convert ${sequelizeType.toSql()} to a GraphQL type`);
+    throw new Error(`Unable to convert ${sequelizeType.key || sequelizeType.toSql()} to a GraphQL type`);
   }
 }
