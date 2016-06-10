ff8bf9f926b9feb8180e2117e8ced60969486731
Loïc Mahieu <mahieuloic@gmail.com>
2016-06-10 11:55:18 +0200

    test: run tests on mysql

--- test/support/helper.js
+++ test/support/helper.js
@@ -5,17 +5,41 @@ export const Promise = Sequelize.Promise
 export const sequelize = createSequelize()
 
 export function createSequelize () {
-  const dialect = process.env.DB === 'pgsql' ? 'postgres' : (process.env.DB || 'sqlite');
-  const host = process.env.DB_PORT_5432_TCP_ADDR || process.env.DB_PORT_3306_TCP_ADDR;
-  let user = 'test';
-  const database = 'test';
-  const password = 'test';
+  const env = process.env;
+  const dialect = env.DIALECT || 'sqlite';
+  const config = Object.assign(
+    {
+      host: 'localhost',
+      user: 'graphql_sequelize_test',
+      password: 'graphql_sequelize_test',
+      database: 'graphql_sequelize_test'
+    },
+    dialect === 'postgres' && {
+      host: env.POSTGRES_PORT_5432_TCP_ADDR,
+      user: env.POSTGRES_ENV_POSTGRES_USER,
+      password: env.POSTGRES_ENV_POSTGRES_PASSWORD,
+      database: env.POSTGRES_ENV_POSTGRES_DATABASE
+    },
+    dialect === 'mysql' && {
+      host: env.MYSQL_PORT_3306_TCP_ADDR,
+      user: env.MYSQL_ENV_MYSQL_USER,
+      password: env.MYSQL_ENV_MYSQL_PASSWORD,
+      database: env.MYSQL_ENV_MYSQL_DATABASE
+    },
+    dialect === 'postgres' && env.CI && {
+      user: 'postgres',
+      password: '',
+      database: 'test'
+    },
+    dialect === 'mysql' && env.CI && {
+      user: 'travis',
+      password: '',
+      database: 'test'
+    }
+  )
 
-  if (dialect === 'postgres' && process.env.CI)
-    user = 'postgres';
-
-  return new Sequelize(database, user, password, {
-    host: host,
+  return new Sequelize(config.database, config.user, config.password, {
+    host: config.host,
     dialect: dialect,
     logging: false
   });
