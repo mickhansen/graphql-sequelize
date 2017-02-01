3b4fbb31b064b84eb66b2451475a5d872c4de9c8
klime <kliment.lambevski@gmail.com>
2017-02-01 18:13:43 +0100

    Update graphql to ^0.9.0 and add temporary name for EnumType in type mapper

--- src/typeMapper.js
+++ src/typeMapper.js
@@ -86,6 +86,7 @@ export function toGraphQL(sequelizeType, sequelizeTypes) {
 
   if (sequelizeType instanceof ENUM) {
     return new GraphQLEnumType({
+      name: 'tempEnumName', //grpahql 0.9 requires name for EnumType. Using tempEnumName until its replaced with field generated name
       values: sequelizeType.values.reduce((obj, value) => {
         let sanitizedValue = value;
         if (specialChars.test(value)) {
