f7fd949d35fc054d3a3718b9b51ac981d37baf03
Aleksey <alekseyb@vlsenergy.com>
2017-08-10 01:35:40 +0300

    configure async-to-bluebird to work according to async/await spec

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// configure async-to-bluebird for await to work according to async/await spec
+import Promise from 'bluebird';
+Promise.coroutine.addYieldHandler(v => Promise.resolve(v));
+
 module.exports = {
   argsToFindOptions: require('./argsToFindOptions'),
   resolver: require('./resolver'),
