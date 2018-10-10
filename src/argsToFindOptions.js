460df46c632559d35ab57b4ec0a647b6ba177aa5
xsbchen <xsbchen@qq.com>
2018-10-10 22:36:33 +0800

    bugfix: got wrong find options when args value = 0

--- src/argsToFindOptions.js
+++ src/argsToFindOptions.js
@@ -5,22 +5,24 @@ export default function argsToFindOptions(args, targetAttributes) {
 
   if (args) {
     Object.keys(args).forEach(function (key) {
-      if (key === 'limit' && args[key]) {
-        result.limit = parseInt(args[key], 10);
-      } else if (key === 'offset' && args[key]) {
-        result.offset = parseInt(args[key], 10);
-      } else if (key === 'order' && args[key]) {
-        if (args[key].indexOf('reverse:') === 0) {
-          result.order = [[args[key].substring(8), 'DESC']];
-        } else {
-          result.order = [[args[key], 'ASC']];
+      if (typeof args[key] !== 'undefined') {
+        if (key === 'limit') {
+          result.limit = parseInt(args[key], 10);
+        } else if (key === 'offset') {
+          result.offset = parseInt(args[key], 10);
+        } else if (key === 'order') {
+          if (args[key].indexOf('reverse:') === 0) {
+            result.order = [[args[key].substring(8), 'DESC']];
+          } else {
+            result.order = [[args[key], 'ASC']];
+          }
+        } else if (key === 'where') {
+          // setup where
+          result.where = replaceWhereOperators(args.where);
+        } else if (~targetAttributes.indexOf(key)) {
+          result.where = result.where || {};
+          result.where[key] = args[key];
         }
-      } else if (key === 'where' && args[key]) {
-        // setup where
-        result.where = replaceWhereOperators(args.where);
-      } else if (~targetAttributes.indexOf(key)) {
-        result.where = result.where || {};
-        result.where[key] = args[key];
       }
     });
   }
